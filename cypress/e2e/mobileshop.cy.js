859177eb8b4d0eae9d0caff4f34b550ca01b31fc
Alexey Bobrikov <a.beaverg@gmail.com>
2024-06-05 12:05:12 +0300

    Updated code

--- cypress/e2e/mobileshop.cy.js
+++ cypress/e2e/mobileshop.cy.js
@@ -2,22 +2,23 @@ let homeUrl = 'https://www.mobileshop.eu/'
 let checkProductPageText = 'Add to basket'
 
 describe('mobileshop.eu tests', () => {
-  const params = require('../fixtures/parameters.json');
+  const params = require('../fixtures/mobile_phones_test_params.json')
+  const locators = require('../fixtures/mobile_phones_test_locators.json')
 
   params.forEach((test) => {
     it(test.testName, () => {
       getHomePage()
-      cy.get('nav[class="main-menu"] button[data-drop="catMainDropPanel"]').should('exist').click({force: true})
-      cy.get(test.dropMenuLocator).should('exist').click({force: true})
-      cy.get('header h1 > span').should('have.text', test.pageTitle)
-      cy.get('div[class="product-wrap"]:first-child div h5 a').as('productTitleElement')
+      cy.get(locators.menuItemLocator).should('exist').click({force: true})
+      cy.get(test.dropMenuItemLocator).should('exist').click({force: true})
+      cy.get(locators.pageTitleLocator).should('have.text', test.pageTitle)
+      cy.get(locators.productTitleLocator1).as('productTitleElement')
       cy.get('@productTitleElement').invoke('text').then($title => {
-        cy.get('div[class="product-wrap"]:first-child div div[class="price"] > div').invoke('text').then($price => {
+        cy.get(locators.productPriceLocator1).invoke('text').then($price => {
           cy.get('@productTitleElement').should('exist').click({force: true})
-          cy.get('div[class="buttons"] > a:first-child > span').should('have.text', checkProductPageText)
-          cy.get('div[class="price-wrap"] > div[class="price"]').invoke('text').should('eq', $price)
+          cy.get(locators.addToBasketButtonLocator).should('have.text', checkProductPageText)
+          cy.get(locators.productPriceLocator2).invoke('text').should('eq', $price)
         })
-        cy.get('h1[class="product-name"]').invoke('text').should('eq', $title) 
+        cy.get(locators.productTitleLocator2).invoke('text').should('eq', $title) 
       })
     })
   })  
@@ -25,6 +26,6 @@ describe('mobileshop.eu tests', () => {
   function getHomePage() {
     cy.visit(homeUrl)
     cy.url().should('include', homeUrl)
-    cy.get('a[class="button db ghost"]').wait(1000).should('exist').click({force: true})
+    cy.get(locators.privacyButtonLocator).wait(1000).should('exist').click({force: true})
   }
 }) 
